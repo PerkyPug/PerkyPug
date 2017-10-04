52fd49250d07fee2a8ce0c90a26b57a9a7e95d99
paulvaldez <valdezpaul00@gmail.com>
2017-10-04 15:09:04 -0500

    Added methods for the exercise.

--- src/util/FileHandler.java
+++ src/util/FileHandler.java
@@ -1,14 +1,11 @@
 package util;
 
-import com.sun.xml.internal.ws.policy.privateutil.PolicyUtils;
-
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.nio.file.StandardOpenOption;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 import java.util.Scanner;
 
