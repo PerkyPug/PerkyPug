d64106cb29b1416077ab54c67b6b80b3ca53a94c
M Coindreau <mariannacoindreau@gmail.com>
2017-10-04 15:02:20 -0500

    working version

--- src/FileHandlerTest.java
+++ src/FileHandlerTest.java
@@ -1,20 +1,2 @@
-import util.FileHandler;
-
-import java.util.ArrayList;
-import java.util.Scanner;
-
 public class FileHandlerTest {
-
-//    Scanner scanner = new Scanner(System.in);
-//        System.out.println("Enter contact name");
-//    String contactName = scanner.nextLine();
-//        System.out.println("Enter contact number");
-//    String phoneNum = scanner.nextLine();
-//        contacts.add(contactName + " | " + phoneNum);
-    FileHandler contactsFile = new FileHandler("contacts", "addContact.txt");
-
-    ArrayList<String> contents = new ArrayList<>();
-
-    //...
-    contactsFile.writeToFile(contents);
 }
