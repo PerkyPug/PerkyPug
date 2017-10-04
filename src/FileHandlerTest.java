b7349be890f228a57b1faf96ce7a7b4d3c5c2f14
M Coindreau <mariannacoindreau@gmail.com>
2017-10-04 16:35:21 -0500

    Completed the first part of the File IO exercise.

--- src/FileHandlerTest.java
+++ src/FileHandlerTest.java
@@ -1,2 +1,28 @@
+import util.FileHandler;
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
 public class FileHandlerTest {
+    public static void main(String[] args) throws IOException {
+        FileHandler contactsFile = new FileHandler("contacts", "contacts.txt");
+        List<String> myContacts = new ArrayList<>();
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter contacts name here: ");
+        String myContactsName = scanner.nextLine();
+        System.out.println("Enter contacts number here:");
+        String phoneNumber = scanner.nextLine();
+        myContacts.add(myContactsName + "," + phoneNumber);
+        contactsFile.writeToFile(myContacts);
+        printAllContacts(contactsFile);
+    }
+    public static void printAllContacts(FileHandler contactsFile) throws IOException {
+        List<String> myFriendsNameAndNumber = contactsFile.readAllContents(); // read all the lines from a file
+        for (String names: myFriendsNameAndNumber) {
+            String[] numbers = names.split(",");
+            System.out.println("Name: " + numbers[0] + " Phone Number: "  + numbers[1]);
+        }
+    }
 }
--- src/util/FileHandler.java
+++ src/util/FileHandler.java
@@ -1,59 +1,35 @@
 package util;
-
+import java.nio.file.*;
 import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Path;
 import java.nio.file.Paths;
-import java.nio.file.StandardOpenOption;
-import java.util.ArrayList;
 import java.util.List;
-import java.util.Scanner;
 
-public class FileHandler {
+    public class FileHandler {
+        private String directory;
+        private String fileName;
+        private Path path;
 
-    private String directory;
-    private String filename;
-
-    //don't forget to handle the exceptions
-
-    public static void main(String[] args) throws IOException{
-
-    // Path to resources/info.txt
-        Path path = Paths.get("contacts", "addContact.txt");       // files
-        System.out.println(path.toAbsolutePath());
-
-        if (!Files.exists(path.getParent())) {
-            Files.createDirectory(path.getParent());
-        }
-        if (!Files.exists(path)) {
-            Files.createFile(path);
+        public FileHandler(String directory, String fileName) throws IOException {
+            this.directory = directory;
+            this.fileName = fileName;
+            this.createFile();
         }
 
-        // Helper methods // static methods
-        //List<String> groceries = Arrays.asList("coffee,3", "tea,5", "sugar,6");
-        List<String> contacts = new ArrayList<>();
-//        contacts.add("coffee,3");
-//        contacts.add("tea,4");
-//        contacts.add("sugar,5");
+        private void createFile() throws IOException {
+            this.path = Paths.get(this.directory, this.fileName); // files
+            if (!Files.exists(path.getParent())) {
+                Files.createDirectory(path.getParent());
+            }
+            if (!Files.exists(path)) {
+                Files.createFile(path);
+            }
+        }
 
-        // serialization  object -> text / deserialization text -> object
+        public List<String> readAllContents() throws IOException {
+            return Files.readAllLines(this.path);
+        }
 
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("Enter contact name");
-        String contactName = scanner.nextLine();
-        System.out.println("Enter contact number");
-        String phoneNum = scanner.nextLine();
-        contacts.add(contactName + " | " + phoneNum);
-
-        Files.write(path, contacts, StandardOpenOption.APPEND);  // write several lines to a file
-
-//        Create a method for retrieving file contents as a List of Strings
-
-        List<String> mySavedContacts = Files.readAllLines(path); // read all the lines from a file
-        System.out.println("Name | Phone number\n" +
-                "---------------\n " + mySavedContacts + "\n");
-
+        public void writeToFile(List<String> contents) throws IOException {
+            Files.write(this.path, contents, StandardOpenOption.APPEND);  // write several lines to a file
+        }
     }
-
-
-}
